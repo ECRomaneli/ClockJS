c1d8eab891e2d90465ca95b3f24fb92d5ec24da2
Emerson Capuchi Romaneli <emerson.romaneli@gmail.com>
2018-06-04 13:00:48 -0300

    Configure build and .ignore

--- build.sh
+++ build.sh
@@ -1,5 +1,37 @@
-echo 'tsc...'
-tsc
-# echo 'uglifyjs...'
-# uglifyjs --compress --mangle --output dist/clock.min.js -- dist/clock.js
-echo 'done!'
+#!/bin/sh
+
+# LIBRARY PATH
+LIB_PATH='./lib/'
+
+# ORIG
+TMP_PATH="${LIB_PATH}tmp/"
+
+# DEST
+WEB_PATH='./dist/web/'
+WEB_TARGET='es5'
+
+# Create module
+if  [ "$1" != 'web' ]; then
+    echo 'Building module...'
+    tsc
+fi
+
+# Create web lib
+if  [ "$1" != 'module' ]; then
+
+    echo 'Building web library...'
+    mkdir ${TMP_PATH}
+    cp -f ${LIB_PATH}*.ts ${TMP_PATH}
+    sed -i s/^[^A-Za-z0-9]*export\\s//g ${TMP_PATH}*.ts
+    tsc ${TMP_PATH}*.ts --module none --target ${WEB_TARGET} --outDir ${WEB_PATH}
+    rm -rf ${TMP_PATH}
+
+    echo 'Minifying...'
+    rm -f ${WEB_PATH}*.min.js
+    for JS in ${WEB_PATH}*.js; do
+        MIN_JS=$(echo $JS | sed s/.js/.min.js/g)
+        uglifyjs --compress --mangle --output $MIN_JS -- $JS
+    done
+fi
+
+echo 'Build finish!'
